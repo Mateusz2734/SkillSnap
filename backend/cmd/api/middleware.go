00b251f185e2203376593fa6604f58fb1f0452f2
Mateusz Wala <mateusz.wala2734@gmail.com>
2023-12-20 22:52:11 +0100

    fix: Update headers

--- backend/cmd/api/middleware.go
+++ backend/cmd/api/middleware.go
@@ -141,9 +141,9 @@ func (app *application) requireAdminPrivileges(next http.Handler) http.Handler {
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if r.Method == "OPTIONS" {
