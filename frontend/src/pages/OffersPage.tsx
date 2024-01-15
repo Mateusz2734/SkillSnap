7aac4bdd76029eaf07c7a313b8cc0741a3b4cb8c
Mateusz Wala <mateusz.wala2734@gmail.com>
2024-01-16 00:01:34 +0100

    feat: Center button, optimize imports

--- frontend/src/pages/OffersPage.tsx
+++ frontend/src/pages/OffersPage.tsx
@@ -1,60 +1,47 @@
 import { useState } from "react";
-import {
-  Button,
-  Modal,
-  ModalDialog,
-  DialogTitle,
-  DialogContent,
-  Grid
-} from "@mui/joy";
+import Button from "@mui/joy/Button";
+import Modal from "@mui/joy/Modal";
+import ModalDialog from "@mui/joy/ModalDialog";
+import DialogTitle from "@mui/joy/DialogTitle";
+import DialogContent from "@mui/joy/DialogContent";
+import Grid from "@mui/joy/Grid";
+import Stack from "@mui/joy/Stack";
+import Add from "@mui/icons-material/Add";
 
 import { useGetOffers } from "../api/offers";
-import { Spinner } from "../components/Spinner";
 import { OfferCard } from "../components/OfferCard";
 import { OfferForm } from "../components/OfferForm";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const Offers = () => {
   const [open, setOpen] = useState<boolean>(false);
-  const { data, error, isLoading } = useGetOffers();
-
-  if (isLoading) return <Spinner />;
-
-  if (error)
-    return (
-      <>
-        <p>There was an error fetching the offers</p>
-        {error.message && <p>The error message: {error.message}</p>}
-      </>
-    );
-
-  if (!data || data.offers.length === 0) return <p>No offers found</p>;
+  const { data } = useGetOffers();
 
   return (
     <>
-      <Button
-        variant="outlined"
-        color="neutral"
-        startDecorator={<FontAwesomeIcon icon={faPlus} />}
-        onClick={() => setOpen(true)}
-      >
-        New Offer
-      </Button>
-      <Modal keepMounted open={open} onClose={() => setOpen(false)}>
-        <ModalDialog>
-          <DialogTitle>Create new offer</DialogTitle>
-          <DialogContent>Fill in the information of the offer.</DialogContent>
-          <OfferForm />
-        </ModalDialog>
-      </Modal>
+      <Stack direction="row" justifyContent="center" sx={{ marginBottom: "2em" }}>
+        <Button
+          variant="outlined"
+          color="neutral"
+          startDecorator={<Add />}
+          onClick={() => setOpen(true)}
+        >
+          New Offer
+        </Button>
+
+        <Modal keepMounted open={open} onClose={() => setOpen(false)}>
+          <ModalDialog>
+            <DialogTitle>Create new offer</DialogTitle>
+            <DialogContent>Fill in the information of the offer.</DialogContent>
+            <OfferForm />
+          </ModalDialog>
+        </Modal>
+      </Stack>
 
       <Grid container spacing={4} >
         {data?.offers.map((offer) => (
           <Grid key={offer.offerId} sx={{ flexGrow: 1, display: "flex", justifyContent: "center", alignItems: "center" }} >
             <OfferCard offer={offer} />
           </Grid>
-
         ))}
       </Grid>
     </>
